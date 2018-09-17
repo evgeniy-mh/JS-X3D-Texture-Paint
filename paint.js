327aa8a2d61734b20846263c10d5887d8568c266
evgeniy-mh <djpon94@gmail.com>
2018-09-17 07:39:22 +0300

    added select new model option

--- paint.js
+++ paint.js
@@ -5,6 +5,7 @@ var clearButton = document.getElementById('clearCanvas'); //ссылка на к
 var colorSelector = document.getElementById('colorSelector'); //ссылка на элемент выбора цвета кисти
 var lineWidthInput = document.getElementById('lineWidthInput'); //ссылка на элемент выбора толщины линии кисти
 var loadNewModelButton = document.getElementById('loadNewModel');
+var ShapeSelectListBox = document.getElementById('ShapeSelectListBox');
 
 var lineColor = "#ff0000"; //начальный цвет кисти
 var myLineSize = 10; //начальная толщина линии
@@ -15,7 +16,6 @@ initCanvas = function () {
 
     //инициализация обработчика события нажатия на холст
     canvas.onmousedown = function (event) {
-        console.log("sadasdasd");
         //инициализация обработчика события передвижения мыши по холсту
         canvas.onmousemove = function (event) {
             var x = event.offsetX;
@@ -25,7 +25,6 @@ initCanvas = function () {
             context.fillStyle = lineColor;
             context.fillRect(x - myLineSize / 2, y - myLineSize / 2, myLineSize, myLineSize);
             context.fill();
-
             canvas.parentNode._x3domNode.invalidateGLObject();
         }
         //инициализация обработчика события отпускания кнопки мыши
@@ -49,64 +48,15 @@ function setAttributes(el, attrs) {
 
 loadNewModelButton.onclick = function () {
 
-    /*var X3DTableTr = document.getElementById("X3DTableTr");
-    var X3DScene = document.getElementById("X3DScene");
-
-    X3DTableTr.removeChild(X3DScene);
-
-    var x3D = document.createElement("x3d");
-    setAttributes(x3D, {
-        "id": "X3DScene",
-        "showLog": "false",
-        "x": "0px",
-        "y": "0px",
-        "width": "500px",
-        "height": "350px",
-    });
-
-
-    var scene = x3D.appendChild(document.createElement("scene"));
-    scene.appendChild(document.createElement("background")).setAttribute("skyColor", "0.15 0.15 0.15");
-    scene.appendChild(document.createElement("viewpoint")).setAttribute("position", "0 0 5");
-
-    var shape = scene.appendChild(document.createElement("shape"));
-    var appearance = shape.appendChild(document.createElement("appearance"));
-    var texture= appearance.appendChild(document.createElement("texture"));
-    texture.setAttribute("hideChildren", "false");
-
-    var canvas=texture.appendChild(document.createElement("canvas"));
-    setAttributes(canvas, {
-        "width":"256",
-        "height":"256",
-        "id":"textureCanvas",
-        "style":"border: solid 1px black; position:absolute; top:20px;left:520px;"
-    });
-    appearance.appendChild(document.createElement("material")).setAttribute("diffuseColor","0 0 0");
-
-    shape.appendChild(document.createElement("sphere"));
-    X3DTableTr.appendChild(x3D);
-
-    initCanvas();*/
-
-    var ot = document.getElementById('X3DTransformRoot');
-    for (var i = 0; i < ot.childNodes.length; i++) {
-        if (ot.childNodes[i].nodeType === Node.ELEMENT_NODE) {
-            ot.removeChild(ot.childNodes[i]);
-            break;
-        }
-    }
-
+    clearX3DViev();
 
     var X3DTransform = document.createElement('transform');
     X3DTransform.setAttribute("translation", "0 0 0");
     var X3DShape = document.createElement('shape');
-
     var X3DAppearance = document.createElement('appearance');
     var X3DMaterial = document.createElement('material');
-
     var texture = X3DAppearance.appendChild(document.createElement("texture"));
     texture.setAttribute("hideChildren", "false");
-
     var canvas = texture.appendChild(document.createElement("canvas"));
     setAttributes(canvas, {
         "width": "256",
@@ -116,17 +66,30 @@ loadNewModelButton.onclick = function () {
     });
 
     X3DAppearance.appendChild(X3DMaterial);
-
     X3DShape.appendChild(X3DAppearance);
-
     X3DTransform.appendChild(X3DShape);
-    var b = document.createElement('sphere');
-    X3DShape.appendChild(b);
 
-    var ot = document.getElementById('X3DTransformRoot');
-    ot.appendChild(X3DTransform);
-    
+    var X3DModel = document.createElement(getX3DModel());
+
+    X3DShape.appendChild(X3DModel);
+
+    X3DTransformRoot = document.getElementById('X3DTransformRoot');
+    X3DTransformRoot.appendChild(X3DTransform);
     initCanvas();
+}
+
+function clearX3DViev() {
+    var X3DTransformRoot = document.getElementById('X3DTransformRoot');
+    for (var i = 0; i < X3DTransformRoot.childNodes.length; i++) {
+        if (X3DTransformRoot.childNodes[i].nodeType === Node.ELEMENT_NODE) {
+            X3DTransformRoot.removeChild(X3DTransformRoot.childNodes[i]);
+            break;
+        }
+    }
+}
+
+function getX3DModel(){
+    return ShapeSelectListBox.options[ShapeSelectListBox.selectedIndex].attributes.name.value;
 }
 
 //инициализация обработчика события нажатия кнопки очистки холста
